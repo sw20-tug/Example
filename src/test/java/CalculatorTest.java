3dea27dd171613717a57f16394e07104f1982af8
Michael Herold <herold@ist.tugraz.at>
2020-03-10 12:43:55 +0100

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,9 @@ public class CalculatorTest {
     public void testAdd() {
         assertEquals(4, calculator.add(3, 1));
     }
+    
+    @Test
+    public void testAddInvalid() {
+        assertEquals(8, calculator.add(3, 1));
+    }
 }
